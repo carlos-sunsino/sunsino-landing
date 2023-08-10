2409222559aa21137982fb438d1764280aa479be
Carlos <carlos@sunsino.com.tw>
2023-08-10 18:07:05 +0800

    feat: update banner bg

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -19,120 +19,130 @@ const { t, locale } = useI18n()
 
 <style lang="scss" scoped>
 #banner {
-    position: relative;
+	position: relative;
 
-    display: flex;
-    flex-direction: column;
+	display: flex;
+	flex-direction: column;
 
-    box-sizing: border-box;
-    padding-top: 100px;
+	box-sizing: border-box;
+	padding-top: 100px;
 
-    @include web() {
-        height: 100vh;
-        max-height: 800px;
-        margin-bottom: -80px;
-        &::before {
-            top: 0;
-            left: 50%;
+	@include web() {
+		height: 100vh;
+		max-height: 800px;
+		margin-bottom: -80px;
+		&::before,
+		&::after {
+			width: 922px;
+			height: 922px;
 
-            transform: translateX(calc(-50% - 600px));
-        }
-        &::after {
-            top: -300px;
-            left: 50%;
+			border-radius: 922px;
+		}
+		&::before {
+			top: 0;
+			left: 50%;
 
-            transform: translateX(calc(-50% + 600px));
-        }
-    }
-    @include phone() {
-        height: 700px;
-        margin-bottom: 70px;
-        &::before {
-            top: -40%;
-            left: -100%;
-        }
-        &::after {
-            right: -150%;
-            bottom: -40%;
-        }
-    }
+			transform: translateX(calc(-50% - 600px));
+		}
+		&::after {
+			top: -300px;
+			left: 50%;
 
-    &::before,
-    &::after {
-        position: absolute;
-        z-index: 10;
+			transform: translateX(calc(-50% + 600px));
+		}
+	}
+	@include phone() {
+		height: 700px;
+		margin-bottom: 70px;
 
-        width: 922px;
-        height: 922px;
+		&::before,
+		&::after {
+			width: 485px;
+			height: 485px;
 
-        content: '';
+			border-radius: 485px;
+		}
+		&::before {
+			top: -20%;
+			left: -50%;
+		}
+		&::after {
+			right: -50%;
+			bottom: -20%;
+		}
+	}
 
-        opacity: .2;
-        border-radius: 922px;
-        background: radial-gradient(50% 50% at 50% 50%, #00ffff 0%, rgba(0, 255, 255, 0) 100%);
-    }
+	&::before,
+	&::after {
+		position: absolute;
+		z-index: 10;
+
+		content: '';
+
+		opacity: 0.2;
+		background: radial-gradient(50% 50% at 50% 50%, #00ffff 0%, rgba(0, 255, 255, 0) 100%);
+	}
 }
 .main-info,
 .main-cover {
-    img {
-        @include web() {
-            height: 100%;
-        }
-        @include phone() {
-            width: 100%;
-        }
-    }
+	img {
+		@include web() {
+			height: 100%;
+		}
+		@include phone() {
+			width: 100%;
+		}
+	}
 }
 .main-info {
-    z-index: 1000;
+	z-index: 1000;
 
-    @include web() {
-        height: 45%;
+	@include web() {
+		height: 45%;
 
-        transform: translateY(30px);
-    }
-    @include phone() {
-        width: 100vw;
+		transform: translateY(30px);
+	}
+	@include phone() {
+		width: 100vw;
 
-        transform: translateY(60px);
-    }
+		transform: translateY(60px);
+	}
 }
 .main-cover {
-    @include web() {
-        height: 70%;
+	@include web() {
+		height: 70%;
 
-        transform: translateY(-180px);
-    }
-    @include phone() {
-        width: 100vw;
-    }
+		transform: translateY(-180px);
+	}
+	@include phone() {
+		width: 100vw;
+	}
 }
 .main-link {
-    position: relative;
-    z-index: 10;
+	position: relative;
+	z-index: 10;
 
-    flex: 0;
+	flex: 0;
 
-    height: 90px;
-    padding-top: 20px;
+	height: 90px;
+	padding-top: 20px;
 
-    button {
-        &::before {
-            position: absolute;
-            z-index: -1;
-            top: 20px;
+	button {
+		&::before {
+			position: absolute;
+			z-index: -1;
+			top: 20px;
 
-            width: 90vw;
-            height: 100px;
+			width: 90vw;
+			height: 100px;
 
-            content: '';
+			content: '';
 
-            background-image: url('/banner-link.png');
-            background-repeat: no-repeat;
-            background-position: center center;
-            background-size: contain;
-        }
-    }
+			background-image: url('/banner-link.png');
+			background-repeat: no-repeat;
+			background-position: center center;
+			background-size: contain;
+		}
+	}
 }
-
 </style>
